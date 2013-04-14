76d55e55fdb703a7c02ead4282992b8c04236967
Brenden Knauss <brenden.knauss@me.com>
2013-04-14 10:54:16 -0700

    Initial Commit

---
+++ objcMaze/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  objcMaze
+//
+//  Created by Brenden Knauss on 4/14/13.
+//  Copyright (c) 2013 Brenden Knauss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
